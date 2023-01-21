5a284daa438bf67f7c3d0e107721925da6a77c1b
cgud3061 <cgud3061@mail4.doshisha.ac.jp>
2023-01-21 11:52:28 +0900

    トップページの修正

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -13,9 +13,9 @@ defineProps({
     <Head title="Welcome" />
 
     <div
-        class="relative flex items-top justify-center min-h-screen bg-hero-img bg-cover sm:items-center sm:pt-0"
+        class="relative flex items-center justify-center min-h-screen bg-hero-img bg-cover sm:items-center sm:pt-0"
     >
-        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+        <div v-if="canLogin" class="fixed top-0 right-0 px-6 py-4 block">
             <Link
                 v-if="$page.props.auth.user"
                 :href="route('dashboard')"
@@ -36,7 +36,24 @@ defineProps({
         </div>
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <img src="https://res.cloudinary.com/dxe6weecx/image/upload/v1673686725/logo_e29jid.png" class="block h-60 w-60">
+            <div class="flex justify-center w-full">
+                <img src="https://res.cloudinary.com/dxe6weecx/image/upload/v1673686725/logo_e29jid.png" class="block h-60 w-60">
+            </div>
+            <div class="w-full mt-10">
+                <div class="flex justify-center w-full">
+                    <h1 class="text-xl text-white bg-black bg-opacity-70 rounded p-2 font-medium mx-3 mt-2">いつの間にか着ない服が増えていた...</h1>
+                </div>
+                <div class="flex justify-center w-full">
+                    <h1 class="text-xl text-white bg-black bg-opacity-70 rounded p-2 font-medium mx-3 my-2">自分のコーディネートに自信がない...</h1>
+                </div>
+                <div class="flex justify-center w-full">
+                    <h1 class="text-xl text-white bg-black bg-opacity-70 rounded p-2 font-medium mx-3 mb-2">マイクロは洋服管理機能とコーディネート投稿機能で</h1>
+                </div>
+                <div class="flex justify-center w-full">
+                    <h1 class="text-xl text-white bg-black bg-opacity-70 rounded p-2 font-medium mx-3 mb-2">そんなあなたのお悩みを解決します！</h1>
+                </div>
+            </div>
         </div>
+        
     </div>
 </template>
